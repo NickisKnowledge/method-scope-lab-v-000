7e0614ce23b159110c1d41cc0e203dfb58cedb42
Z Drake <zeedrake3000@gmail.com>
2018-03-26 22:18:26 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,6 +3,7 @@
 # rescue_princess_peach
 # puts status
 
-def rescue_princess_peach(phrase)
-  puts phrase
+def rescue_princess_peach
+  status = 'rescued'
+  puts "Hooray! Mario has rescued Princess Peach."
 end
